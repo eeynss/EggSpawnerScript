5e19a29f7b59d4c22fb1b43d72f0287477b1c59b
eeynss <104753643+eeynss@users.noreply.github.com>
2025-07-13 21:33:52 +0800

    main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/wjfXFofX/raw"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
